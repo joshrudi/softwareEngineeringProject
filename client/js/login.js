d525dc2afec31ecf1c080648c6324631b4719df8
warjr0000 <warjr0000@hotmail.com>
2019-03-30 13:57:35 -0400

    Removed auto redirect

--- client/js/login.js
+++ client/js/login.js
@@ -1,7 +1,7 @@
 function onSignIn(googleUser) {
 	var id_token = googleUser.getAuthResponse().id_token;
 	write_cookie(id_token);
-	window.location.href = "/index.html"; // Redirect
+	//window.location.href = "/index.html"; // Redirect
 }
 
 function signOut() {
