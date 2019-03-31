6188606a5297d192aeec95e0b0f349d2af703d1d
Joshua Rudaitis <joshr@Joshuas-MacBook-Air.local>
2019-03-30 23:40:00 -0400

    added share/export functionality to index.html, clicking on share FAB will generate an email with current data

--- client/js/controllers/regionController.js
+++ client/js/controllers/regionController.js
@@ -7,6 +7,24 @@ angular.module('regions').controller('RegionsController', ['$scope', 'Regions',
 	$scope.WOEID_clicked = function(woeid, name) {
 		update_chart(woeid, name);
 	}
+
+	$scope.share = function() {
+
+	    var d = new Date();
+	    var month = d.getMonth() + 1;
+	    var date = '' + month + '/' + d.getDate() + '/' + d.getFullYear();
+	    //var query = document.getElementById('regionSearch').value;
+	    var data = '';
+
+	    for (var i = 0; i < $scope.regions.length; i++) data += $scope.regions[i].woeid + new Array(13 - $scope.regions[i].woeid.toString().length).join(' ') + $scope.regions[i].name + '\n';
+	    data = encodeURIComponent(data);
+
+	    //if (query === undefined || query === '') query = 'Worldwide';
+	    var email = 'mailto:' + '?subject=' + 'Twitter Analytics Trending Data for ' + date + '&body=' + data;
+
+	    // encode for URL
+	    window.open(email,"_self");
+	}
   }
 ]);
 
