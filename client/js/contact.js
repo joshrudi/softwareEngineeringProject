e1f93bbaa41025bfb00a8be9baf6daa4fb5821a4
Joshua Rudaitis <joshr@Joshuas-MacBook-Air.local>
2019-03-31 03:15:40 -0400

    fixed issue in contact.js where multiple emails could be sent back-to-back even with empty fields

--- client/js/contact.js
+++ client/js/contact.js
@@ -1,15 +1,24 @@
+var isSent = false;
+
 function send_my_email() {
-	$.ajax({
-		url: "/send_email",
-		type: "POST",
-		data: {
-			name: $("#inputName")[0].value,
-			email: $("#inputEmail")[0].value,
-			issues: $("#inputIssues")[0].value
-		},
-		success: function(data){}
-	});
-	
+
+	if ($("#inputName")[0].value === '' || $("#inputEmail")[0].value === '' || $("#inputIssues")[0].value === '') isSent = false;
+	else {
+
+		isSent = true;
+
+			$.ajax({
+			url: "/send_email",
+			type: "POST",
+			data: {
+				name: $("#inputName")[0].value,
+				email: $("#inputEmail")[0].value,
+				issues: $("#inputIssues")[0].value
+			},
+			success: function(data){}
+		});
+	}
+
 	$("#inputName")[0].value = "";
 	$("#inputEmail")[0].value = "";
 	$("#inputIssues")[0].value = "";
@@ -21,7 +30,8 @@ function send_my_email() {
   var showToastButton = document.querySelector('#show-toast');
   showToastButton.addEventListener('click', function() {
     'use strict';
-    var data = {message: 'Feedback Sent!'};
+    if (isSent) var data = {message: 'Feedback Sent!'};
+    else var data = {message: 'Uh oh! Error!'};
     snackbarContainer.MaterialSnackbar.showSnackbar(data);
   });
 }());
