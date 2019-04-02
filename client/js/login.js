069d9f41053c6f66e06f6f8f528debb7f30675ec
warjr0000 <warjr0000@hotmail.com>
2019-04-02 15:42:36 -0400

    Attempting onLoad initialization

--- client/js/login.js
+++ client/js/login.js
@@ -23,11 +23,13 @@ function onSignIn(googleUser) {
 	window.location.href = "/index.html"; // Redirect
 }
 
-function signOut() {
+function onLoad() {
 	gapi.load('auth2', function() {
 		gapi.auth2.init();
 	});
+}
 
+function signOut() {
 	var auth2 = gapi.auth2.getAuthInstance();
 	auth2.signOut().then(function () {
 		console.log('User signed out.');
