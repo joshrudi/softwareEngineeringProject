238530cf6ca6a2eae6bf2806aeec734e07c2bd69
warjr0000 <warjr0000@hotmail.com>
2019-04-12 15:30:10 -0400

    Testing uh oh

--- client/js/contact.js
+++ client/js/contact.js
@@ -4,26 +4,31 @@ var success_data = {message: 'Feedback Sent!'};
 var error_data = {message: 'Uh oh! Error!'};
 
 function send_my_email() {
-	var cookie = read_cookie();
+	if ($("#inputName")[0].value === '' || $("#inputEmail")[0].value === '' || $("#inputIssues")[0].value === '') {
+		snackbarContainer.MaterialSnackbar.showSnackbar(error_data);
+	} else {
 
-	$.ajax({
-		url: "/send_email",
-		type: "POST",
-		data: {
-			name: $("#inputName")[0].value,
-			email: $("#inputEmail")[0].value,
-			issues: $("#inputIssues")[0].value,
-			user_id: cookie.user_id,
-		},
-		success: function(data){
-			snackbarContainer.MaterialSnackbar.showSnackbar(success_data);
+		var cookie = read_cookie();
 
-			$("#inputName")[0].value = "";
-			$("#inputEmail")[0].value = "";
-			$("#inputIssues")[0].value = "";
-		},
-		error: function() {
-			snackbarContainer.MaterialSnackbar.showSnackbar(error_data);
-		}
-	});
+		$.ajax({
+			url: "/send_email",
+			type: "POST",
+			data: {
+				name: $("#inputName")[0].value,
+				email: $("#inputEmail")[0].value,
+				issues: $("#inputIssues")[0].value,
+				user_id: cookie.user_id,
+			},
+			success: function(data){
+				snackbarContainer.MaterialSnackbar.showSnackbar(success_data);
+
+				$("#inputName")[0].value = "";
+				$("#inputEmail")[0].value = "";
+				$("#inputIssues")[0].value = "";
+			},
+			error: function() {
+				snackbarContainer.MaterialSnackbar.showSnackbar(error_data);
+			}
+		});
+	}
 }
